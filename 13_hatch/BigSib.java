c85b1bbe20f08d202774f06e821069ab166b2351
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2021-10-07 00:13:47 -0400

    Create BigSib.java

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,9 @@
+public class BigSib{
+    String mmsg = new String();
+    public BigSib() {
+        System.out.print("whats up ");
+    }
+    public static String greet(String mmsg) {
+    return mmsg;
+    }
+}
