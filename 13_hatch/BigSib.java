bb999518fc875800e8e509924c415e8cb51690e5
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2021-10-07 00:31:27 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,3 +1,17 @@
+/*
+Jonathan Song (with Phoenix) and Oscar Breen (with Redhead)
+APCS
+HW13 -- Where Do BigSibs Come From?
+2021-10-06
+*/
+
+/*
+DISCO:
+ - You can store the constructor as anything, so long as you recall it.
+QCC:
+ - Can constructors be created in the helper class?
+*/
+
 public class BigSib{
     String mmsg = new String();
     public BigSib() {
