4324195cbfbe4ee8fb1d7e6c484c1894551a5633
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2021-10-07 23:31:53 -0400

    Create BigSib.java

---
+++ 14_overload/defcon/BigSib.java
@@ -0,0 +1,26 @@
+/*
+Jonathan Song (with Phoenix), Oscar Breen (with Redhead), and Abdullah Faruque (with Green Lantern)
+APCS
+HW14 -- Customize Your Creation
+2021-10-07
+*/
+
+/*
+DISCO:
+ - The default constructor can be overwritten
+ - The default constructor doesn't take a paramter, while the overloaded constructor does 
+QCC:
+ - What is the difference between using a default constructor and using a overloaded constructor?
+*/
+
+public class BigSib{
+	String mmsg;
+
+  public BigSib(){
+	mmsg = "Word up";
+  }
+
+  public String greet(String name){
+    	return( mmsg + " " + name );
+  }
+ }
