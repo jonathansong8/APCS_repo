744179ee3b95ef597cd142d21b778dfeb963343b
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2021-10-18 07:51:48 -0400

    Update bankaccount.java

--- 18_BankAccount/bankaccount.java
+++ 18_BankAccount/bankaccount.java
@@ -4,14 +4,17 @@ APCS
 HW18 - CPA-one
 2021-10-14
 QCC: When do we use static and when do we not? What does static do exactly? When is it a good idea to make instance variables/methods private or public?
+
 DISCO: I initially did not include static with my instance variables and when I tried to run my code, I got this:
 error: non-static variable fullName cannot be referenced from a static context
 I'm still now sure exactly what static does and when we should use it. 
 I also found that using a constructor would be more efficient than setting up methods to set each individual variable, but there may be times 
 when using individual methods for each variable would be better. 
 
-Lastest and Greatest Q2 Response:
-Latest and Greatest Q3 Response:
+Lastest and Greatest Q2 Response: We know that before we write our own constructor, Java provides us with a constructor because when we create a new object using 
+ClassName objectName = new ClassName() and then we try to print out the instance variables of that objectName, the output is null for Strings and 0 for primitives.
+
+Latest and Greatest Q3 Response:To try to figure out if it's possible to output a string representation of an object, we can try to run System.out.println(objectName). 
 */
 
 public class BankAccount{
@@ -29,7 +32,7 @@ public class BankAccount{
     accountBalance = f;
   }
 
-  public static void infoPrint(){
+  public void infoPrint(){
     System.out.println("Name: "+fullName);
     System.out.println("Password: "+password);
     System.out.println("PIN: "+pin);
@@ -37,23 +40,23 @@ public class BankAccount{
     System.out.println("Account Balance: "+accountBalance+" dollars");
   }
 
-  public static void deposit(double depositAmount){
+  public void deposit(double depositAmount){
     accountBalance = accountBalance + depositAmount;
 
   }
 
-  public static void withdraw(double withdrawAmount){
+  public void withdraw(double withdrawAmount){
     accountBalance = accountBalance - withdrawAmount;
 
   }
 
   public static void main(String[] args){
     BankAccount firstAccount = new BankAccount("Flounder", "you'll never guess", 1234, 1, 10000.99);
-    infoPrint();
-    withdraw(200.49);
-    infoPrint();
-    deposit(2343.53);
-    infoPrint();
+    firstAccount.infoPrint();
+    firstAccount.withdraw(200.49);
+    firstAccount.infoPrint();
+    firstAccount.deposit(2343.53);
+    firstAccount.infoPrint();
 
   }
 
