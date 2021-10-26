649e263c83b7797787a15f4c4f9221e5671c192b
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2021-10-26 00:29:55 -0400

    Update Driver.java

--- 24_while/Driver.java
+++ 24_while/Driver.java
@@ -1,3 +1,21 @@
+/**
+Dancing HotDogs: Lauren Lee, Jonathan Song
+APCS
+HW24: Get it while you can
+2021-10-25
+time spent: 1.5 hrs
+**/
+
+/**
+DISCO
+- There must be two while loops because a conditions like this
+(matchCounter <  65536 && matchCounter % 2005 != 0)
+cannnot take all cases because after the first condition, it will not consider the second condition. 
+QCC
+- what is a way we can use only one while loop.
+even when I used || insted of  &&, I was unable to see all the cases that having two while loops allowed me to. 
+**/
+
 public class Driver {
 
   public static void main( String[] args ) {
@@ -6,7 +24,6 @@ public class Driver {
 
 
     //test default constructor
-  //  /*===================TOP==========================
       Coin mine = new Coin();
       //test 1st overloaded constructor
       Coin yours = new Coin( "quarter" );
@@ -29,8 +46,27 @@ public class Driver {
       else {
         System.out.println( "No match. Firestarter you can not be." );
       }
-//      ====================BOTTOM======================*/
 
+	mine.reset("heads", 0.5);
+	yours.reset("heads",0.5);
+	int headsCounter = 0;
+	int matchCounter = 0;
+	while (matchCounter <= 65536){
+		mine.flip();
+		yours.flip();
+		if (yours.equals(mine) == true) {
+			matchCounter = matchCounter + 1;
+		}
+	}
+	while (matchCounter % 2005 != 0) {
+		mine.flip();
+		yours.flip();
+		if (yours.equals(mine) == true) {
+			matchCounter = matchCounter + 1;
+		}
+	}
+	int totalHeads =  yours.getHeadsCtr() + mine.getHeadsCtr();
+	System.out.println(yours.getHeadsCtr() + mine.getHeadsCtr() + " heads have come up" + "\n" + matchCounter + " matches have occured");
   }//end main()
 
 }//end class
