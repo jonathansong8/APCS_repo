be1711d00ffa1bd64c9053ad82a934b4dcb6d2f1
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2021-10-31 21:42:19 -0400

    Update helloName.java

--- cb/string-1/helloName.java
+++ cb/string-1/helloName.java
@@ -1,4 +1,10 @@
 public String helloName(String name) {
   String message = "Hello " + name + "!";
   return message;
+  
+  	public static void main(String[] args) {
+		System.out.println(helloName("Bob"));
+		System.out.println(helloName("X"));
+		System.out.println(helloName("ho ho ho"));
+	}
 }
