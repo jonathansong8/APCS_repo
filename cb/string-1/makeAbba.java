e742131cf0fe42b5f3295641f85cf4b47c4a2f8b
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2021-10-31 21:42:47 -0400

    Update makeAbba.java

--- cb/string-1/makeAbba.java
+++ cb/string-1/makeAbba.java
@@ -1,4 +1,10 @@
 public String makeAbba(String a, String b) {
   String message = a + b + b + a;
   return message;
+  
+  	public static void main(String[] args) {
+		System.out.println(makeAbba("Hi", "Bye"));
+		System.out.println(makeAbba("Yo", "Alice"));
+		System.out.println(makeAbba("What", "Up") );
+	}
 }
