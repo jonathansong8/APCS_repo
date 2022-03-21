85ddd9404ea90eed864cad61c23c6dea0c0171f0
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2022-03-20 22:55:34 -0400

    Update ThirteensGUIRunner.java

--- cobo/011_elevens/Activity11  Starter Code/ThirteensGUIRunner.java
+++ cobo/011_elevens/Activity11  Starter Code/ThirteensGUIRunner.java
@@ -1,11 +1,17 @@
+/**Team Something Important Minus One: Joshua Yagupsky, Jonathan Song
+APCS pd7
+L07 --  Card Game
+2022-03-20
+time spent: 3.5 hr
+**/
 /**
- * This is a class that plays the GUI version of the Thirteens game.
- * See accompanying documents for a description of how Thirteens is played.
+ * This is a class that plays the GUI version of the Elevens game.
+ * See accompanying documents for a description of how Elevens is played.
  */
 public class ThirteensGUIRunner {
 
 	/**
-	 * Plays the GUI version of Thirteens.
+	 * Plays the GUI version of Elevens.
 	 * @param args is not used.
 	 */
 	public static void main(String[] args) {
