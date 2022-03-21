0554038be1a32eac83ca4d33457750643878c2ea
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2022-03-20 22:26:54 -0400

    Update CardTester.java

--- cobo/011_elevens/Activity1 Starter Code/CardTester.java
+++ cobo/011_elevens/Activity1 Starter Code/CardTester.java
@@ -8,6 +8,23 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card aceHearts = new Card("Ace", "Hearts" , 1);
+		System.out.println(aceHearts.rank());
+		System.out.println(aceHearts.suit());
+		System.out.println(aceHearts.pointValue());
+		System.out.println(aceHearts);
+		Card aceHearts2 = new Card("Ace", "Hearts" , 1);
+		System.out.println(aceHearts2.rank());
+		System.out.println(aceHearts2.suit());
+		System.out.println(aceHearts2.pointValue());
+		System.out.println(aceHearts2);
+		Card queenHearts = new Card("Queen", "Hearts" , 12);
+		System.out.println(queenHearts.rank());
+		System.out.println(queenHearts.suit());
+		System.out.println(queenHearts.pointValue());
+		System.out.println(queenHearts);
+		System.out.println(aceHearts.matches(aceHearts2));
+		System.out.println(queenHearts.matches(aceHearts));
+		System.out.println(aceHearts2.matches(queenHearts));
 	}
 }
