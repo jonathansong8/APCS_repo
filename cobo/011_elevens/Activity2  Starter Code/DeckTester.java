594674c3cbb96ab73710edb05f58afffb47d86cc
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2022-03-20 22:36:39 -0400

    Update DeckTester.java

--- cobo/011_elevens/Activity2  Starter Code/DeckTester.java
+++ cobo/011_elevens/Activity2  Starter Code/DeckTester.java
@@ -1,3 +1,9 @@
+/**Team Something Important Minus One: Joshua Yagupsky, Jonathan Song
+APCS pd7
+L07 --  Card Game
+2022-03-20
+time spent: 3.5 hr
+**/
 /**
  * This is a class that tests the Deck class.
  */
@@ -8,6 +14,28 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] allSuits = {"Hearts", "Clubs", "Diamonds", "Spades"};
+		String[] spadeSuits = {"Spades"};
+		String[] allCards = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+		String[] broadwayCards = {"Ten", "Jack", "Queen", "King", "Ace"};
+		int[] allValues = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+		int[] broadwayValues = {10,11,12,13,14};
+		Deck fullDeck = new Deck(allCards, allSuits, allValues);
+		Deck spadeRoyalFlush = new Deck(broadwayCards, spadeSuits, broadwayValues);
+		System.out.println("–––––––STARTING DECKS–––––––")
+		System.out.println(fullDeck);
+		System.out.println(spadeRoyalFlush);
+		System.out.println("–––––––DEALING ROYAL FLUSH–––––––")
+		while(!spadeRoyalFlush.isEmpty()){
+			System.out.println(spadeRoyalFlush.deal());
+		}
+		System.out.println("–––––––END ROYAL FLUSH–––––––")
+		System.out.println(spadeRoyalFlush);
+		System.out.println("–––––––DEALING FULL DECK–––––––")
+		while(!fullDeck.isEmpty()){
+			System.out.println(fullDeck.deal());
+		}
+		System.out.println("–––––––END FULL DECK–––––––")
+		System.out.println(fullDeck);
 	}
 }
