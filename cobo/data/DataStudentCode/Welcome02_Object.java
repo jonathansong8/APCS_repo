9c1e13a9a857d4d5c22ea345af3b96a5201efca0
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2022-04-06 15:07:02 -0400

    Update Welcome02_Object.java

--- cobo/data/DataStudentCode/Welcome02_Object.java
+++ cobo/data/DataStudentCode/Welcome02_Object.java
@@ -1,52 +1,38 @@
-import core.data.*;
+/*
+ Represents information about a NWS weather station
+*/
 
-public class Welcome02_Object {
-   public static void main(String[] args) {
-      String id1 = "KATL";
-      DataSource ds1 = DataSource.connect("http://weather.gov/xml/current_obs/" + id1 + ".xml"); 
-      ds1.setCacheTimeout(15 * 60);  
-      ds1.load();
-      //ds1.printUsageString();
+public class WeatherStation {
+   private String name;
+   private String id;
+   private String state;
+   private double lat;
+   private double lng;
 
-      Observation ob1 = ds1.fetch("Observation", "weather", "temp_f", "wind_degrees");
-      System.out.println(id1 + ": " + ob1);
-      
-      String id2 = "KSAV";
-      DataSource ds2 = DataSource.connect("http://weather.gov/xml/current_obs/" + id2 + ".xml"); 
-      ds2.setCacheTimeout(15 * 60);  
-      ds2.load();
-      
-      Observation ob2 = ds2.fetch("Observation", "weather", "temp_f", "wind_degrees");
-      System.out.println(id2 + ": " + ob2);
-      
-      if (ob1.colderThan(ob2)) {
-         System.out.println("Colder at " + id1);
-      } else {
-         System.out.println("Colder at " + id2);
-      }
+   WeatherStation(String name, String id, String state, double lat, double lng) {
+      this.name = name;
+      this.id = id;
+      this.lat = lat;
+      this.lng = lng;
+      this.state = state;
    }
-}
 
+   /* Produce the id of this station */
+   public String getId() {
+      return id;
+   }
 
-/* Represents a weather observation */
-class Observation {
-   float temp;    // in fahrenheit
-   int windDir;   // in degrees
-   String description;
-   
-   Observation(String description, float temp, int windDir) {
-      this.description = description;
-      this.temp = temp;
-      this.windDir = windDir;
+   /* Produce the name of this station */
+   public String getName() {
+      return name;
    }
-   
-   /* determine if the temperature of this observation is colder than 'that's */
-   public boolean colderThan(Observation that) {
-      return this.temp < that.temp;
+
+   /* Determine if this weather station is located in the given state */
+   public boolean isLocatedInState(String st) {
+      return this.state.equals(st);
    }
-   
-   /* produce a string describing this observation */
-   public String toString() {
-      return (temp + " degrees; " + description + " (wind: " + windDir + " degrees)");
+
+   public double getLat(){
+     return this.lat;
    }
 }
