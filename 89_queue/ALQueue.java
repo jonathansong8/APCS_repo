e3b7b8630db2d0f4121c9fb4cc96476468928c87
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2022-04-12 09:54:11 -0400

    Create ALQueue.java

---
+++ 89_queue/ALQueue.java
@@ -0,0 +1,45 @@
+/*
+Team Something Important: Joshua Yagupsky, Jaylen Zeng, Jonathan Song
+APCS pd7
+HW89 -- Queue Two Ways
+2022-04-11
+time spent: 0.1 hr
+
+DISCO
+
+QCC
+
+*/
+
+import java.util.ArrayList;
+public class ALQueue<T> implements Queue<T> {
+	private ArrayList<T> _list;
+	public ALQueue(){
+		_list = new ArrayList<T>();
+	}
+	public void enqueue(T val){
+		_list.add(0, val);
+	}
+	public T dequeue(){
+		if(!isEmpty()){
+			return _list.remove(_list.size()-1);
+		}
+		return null;
+	}
+	public T peekFront(){
+		return _list.get(_list.size()-1);
+	}
+	public boolean isEmpty(){
+		return _list.isEmpty();
+	}
+	public static void main(String[] args) {
+		ALQueue<Integer> q = new ALQueue<Integer>();
+		for (int i = 0; i < 80; i+=2 ) {
+			q.enqueue(i);
+		}
+		while (!q.isEmpty()){
+			System.out.println(q.peekFront());
+			q.dequeue();
+		}
+	}
+}
