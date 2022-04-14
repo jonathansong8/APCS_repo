67c7539b5ddfda384b5cec3a7c8b184104b536a0
Jonathan Song <75910900+jonathansong8@users.noreply.github.com>
2022-04-14 06:30:16 -0400

    Update Deque.java

--- 90_deque/Deque.java
+++ 90_deque/Deque.java
@@ -1,10 +1,11 @@
 /*
 Team Something Important: Joshua Yagupsky, Jaylen Zeng, Jonathan Song
 APCS pd7
-HW90 -- Queue Two Ways
-2022-04-11
+HW91 -- Deque the Halls
+2022-04-13
 time spent: 0.2 hr
 */
+import java.util.Iterator;
 public interface Deque<E> extends Iterable<E>{
 	public void addFirst(E val);
 	public void addLast(E val);
@@ -14,4 +15,6 @@ public interface Deque<E> extends Iterable<E>{
 	public E getLast();
 	public boolean isEmpty();
 	public int size();
+	public boolean contains(Object o);
+	public Iterator<E> descendingIterator();
 }
